4c61bf56b819b0727861ac7d504dcce37fbb98ee
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-01-13 20:03:55 +0000

    password generator is working

--- script.js
+++ script.js
@@ -88,31 +88,67 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+let passwordLength = 0;
+let charOpt = false;
+let numOpt = false;
+let lowerCaseOpt = false;
+let upperCaseOpt = false;
 // Function to prompt user for password options
 function getPasswordOptions() {
-  let passwordLength = prompt("please state the length of the password in numbers.");
-  let charOpt = confirm("Would you like special characters in your password?");
-  let numOpt = confirm("Would like the password to contain numbers?");
-  let lowerCaseOpt = confirm("Can the password contain lowercase letter?");
-  let upperCaseOpt = confirm("Do you require uppercase letters in the password?");
+  passwordLength += prompt("please state the length of the password in numbers.");
+  charOpt = confirm("Would you like special characters in your password?");
+  numOpt = confirm("Would like the password to contain numbers?");
+  lowerCaseOpt = confirm("Can the password contain lowercase letter?");
+  upperCaseOpt = confirm("Do you require uppercase letters in the password?");
+
+  console.log(passwordLength)
+
+  let optionArray = [charOpt, numOpt, lowerCaseOpt, upperCaseOpt];
+  console.log(optionArray);
+  
 
  
-  console.log(passwordLength);
+  /*console.log(passwordLength);
   console.log(charOpt);
   console.log(numOpt);
-  console.log(lowerCaseOpt);
+  console.log(lowerCaseOpt);*/
 
 }
 // Function for getting a random element from an array
 function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)] 
+  
 
 }
-
+let newPassword = "";
 // Function to generate password with user input
 function generatePassword() {
   getPasswordOptions();
+  console.log(passwordLength)
+  for (i = 0; i < passwordLength; i++) {
+    if (charOpt) {
+      newPassword += getRandom(specialCharacters);
+      console.log(newPassword);
+    } 
+    if (numOpt) {
+      newPassword += getRandom(numericCharacters);
+    }
+    if (lowerCaseOpt) {
+      newPassword += getRandom(lowerCasedCharacters);
+    }
+    if (upperCaseOpt) {
+      newPassword += getRandom(upperCasedCharacters);
+    }
+  }
+console.log(newPassword)
+return newPassword
+
+  
+  
 
 }
+
+console.log(passwordLength)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
